b70c6bbb87287523221dc375b4491a0b4b4a7901
michellesprojects <69691406+michellesprojects@users.noreply.github.com>
2021-03-14 14:58:17 -0400

    initial commit

---
+++ Ball-Run-Game/app.js
@@ -0,0 +1,36 @@
+var player = document.getElementById("player");
+var container = document.getElementById("container");
+var widthContainer = window.getComputedStyle(container).width;
+var widthContainerInteger = widthContainer.substr(0, widthContainer.length - 2);
+var ballSpeed = 20;
+
+function moveRight() {
+  var left = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
+
+  console.log(widthContainerInteger);
+
+  /* Keep the ball within the boundaries of the container, make sure you cant move past the right bound */
+  if (left + ballSpeed < widthContainerInteger) {
+    player.style.left = left + ballSpeed + "px";
+  }
+}
+
+function moveLeft() {
+  var left = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
+
+  /* Keep the ball within the boundaries of the container, make sure you cant move past the left bound */
+  if (left > 0) {
+    player.style.left = left - ballSpeed + "px";
+  }
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "ArrowLeft") {
+    moveLeft();
+  }
+  if (e.key == "ArrowRight") {
+    moveRight();
+  }
+});
+
+document.addEventListener("keyup", (e) => {});
